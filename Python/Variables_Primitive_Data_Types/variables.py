7b52f05193cfdd8aab80bdcc725e3f4ac3954f37
Aron Rop <aaronrop40@gmail.com>
2025-03-05 11:05:53 +0300

    added a variable.py file

---
+++ Python/Variables_Primitive_Data_Types/variables.py
@@ -0,0 +1,50 @@
+#Variables is a container for storing data values.
+
+#VARIABLES NAMING CONVENTION.
+
+#When naming variables, there are some rules to follow:
+#A variable name must start with a letter or the underscore character.
+# student = "Enock"
+# print(student)
+# _dob ="5-03-1998"
+# print(_dob)
+#A variable name cannot start with a number.
+# 3student = "Enock"
+# print(3student)
+#A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).
+# student1 = "Kirui"
+# student_name = "Kipng'eno"
+# print(student1)
+# print(student_name)
+#Variable names are case-sensitive (age, Age and AGE are three different variables).
+# age = 25
+# print("age:" , age)
+# Age = 30
+# print("Age:" , Age)
+#Variable names cannot contain spaces.
+# student name = "Enock"
+# print(student name)
+#Variable names     should be descriptive and meaningful ( age is better than a).
+# is_student = True
+# print(is_student)
+# is_student_ID = 2345
+# print(is_student_ID)
+#Variable names cannot be reserved keywords in python.
+# global = "world"
+#Variable names should be written in lowercase with underscores between words ( age_of_student is better than ageOfStudent).
+#standard naming conventions are;
+#1. CamelCase
+# studentName = "Enock"
+# print(studentName)
+#2. PascalCase
+# StudentName = "Enock"
+# print(StudentName)
+#3. SnakeCase
+# student_name = "Enock"
+# print(student_name)
+
+#Variable can be overwriten/updated
+# student_ID = 10
+# print(student_ID)
+# student_ID = 20
+# print(student_ID)
