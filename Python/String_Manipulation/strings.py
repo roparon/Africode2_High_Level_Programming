42ccc75d1d9a146e9754c14aebfeeb12abb43a59
Aron Rop <aaronrop40@gmail.com>
2025-03-06 11:13:05 +0300

    created string manipulation file

---
+++ Python/String_Manipulation/strings.py
@@ -0,0 +1,75 @@
+#***********String slicing*******
+# greeting = "Hello,!"
+# name = "John Smith"
+
+# X = greeting + " " + name[0:4]
+# print(X)
+
+#*********Concatenation*******
+# greeting = "Hello "
+# name = "Jane Doe"
+
+# Y = greeting + name
+# print(Y)
+
+# print(help(str))
+
+
+#*********String methods*******
+name = "john smith"
+newname = "John"
+
+
+#1. capitalize METHOD
+# name = "john smith"
+# cap_name = name.capitalize()
+# print(cap_name)
+
+#2. ****Upper() METHOD***
+# upper_name = "john smith"
+# cap_name = upper_name.upper()
+# print(cap_name)
+
+#3. ****Lower() METHOD***
+# lower_name = "JOHN SMITH"
+# cap_name = lower_name.lower()
+# print(cap_name)
+
+#4. ****Count() METHOD***
+# print(name.count('o'))
+phone = "0726-123-456"
+phone_new = ["0726" "123" "456"]
+
+#5.****split() METHOD***
+# new_list = name.split()
+# print(new_list)
+# new_phone = phone.split()
+# print(new_phone)
+
+# new_phone = phone.split('-')
+# print(new_phone)
+
+#*****Not part of this topic******
+# my_name = "Abednego Kipngeno Bett"
+# new_li = my_name.split()
+# middle_name = new_li[1]
+# print(middle_name)
+
+#6.********Join()method********
+# new_list = phone.split()
+# print(new_list)
+# phone = "-".join(new_list)
+# print(phone)
+# joined_phnone = ''.join(phone_new)
+# print(joined_phnone)
+
+# print(type(phone))
+# print(type(joined_phnone))
+
+#7.*****isalnum method******(alphanumeric method)
+# python3 = "python3"
+# print(python3.isalnum())
+
+#8.*****isalpha method******(alphabetic method)
+# python = "python"
+# print(python.isalpha())
