34571be41a9f377848cfc67d2f22e081b06621d3
Aron Rop <aaronrop40@gmail.com>
2025-03-07 15:51:17 +0300

    created some files for demonstration of some list programmes

---
+++ Python/List/creating_list.py
@@ -0,0 +1,31 @@
+#1. With initial items
+
+students = ['John', 'Mary', 'Peter', 'Jane']
+# print(students)
+
+#2. using list() constructor
+fruits = list()
+# print(fruits)
+
+#3. converting string to list
+string = "Hello World"
+# print(string)
+string_list = list(string)
+# print(string_list)
+
+#4. Empty lisT
+attendance = []
+# print(attendance)
+
+#5. pattern list
+pattern = [0] * 1000
+# print(pattern)
+
+#6. list comprehension
+numbers = [x for x in range(1, 11)]
+# print(numbers)
+
+# square = [x for x in range(1, 11) if x % 2 == 0]
+# print(square)
+squares = [i**2 for i in range(1, 11)]
+# print(squares)
