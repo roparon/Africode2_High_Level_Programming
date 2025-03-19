2b44f4561f615a93f74886cc36877c5def18aa82
Aron Rop <aaronrop40@gmail.com>
2025-03-19 14:17:59 +0300

    created a logical operators file

---
+++ Python/Logical_Operators/Logical_operators.py
@@ -0,0 +1,33 @@
+#AND : True when botth operand are true
+
+# is_student = True
+# is_present = True
+
+# print(is_student and is_present)
+
+# is_student = True
+# is_present = False
+
+# print(is_student and is_present)
+
+
+
+#Or : True when atleast one operand is true
+# is_student = False
+# is_present = False
+
+# print(is_student or is_present)
+# is_student = False
+# is_present = True
+
+# print(is_student or is_present)
+# is_student = True
+# is_present = True
+
+# print(is_student or is_present)
+
+
+#Not : True when operand is false
+
+is_student = False
+print( not is_student)
