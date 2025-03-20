a96ab9d0a11ec9cda55321d588931f538bc187a8
Aron Rop <aaronrop40@gmail.com>
2025-03-20 13:21:13 +0300

    created some other exercise files

---
+++ Python/Cal_Exercise/calc_asssignment.py
@@ -0,0 +1,19 @@
+print("Hello, welcome to Rop's calculator!")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum = num1 + num2
+print("The sum is:", sum)
+
+difference = num1 - num2
+print("The difference is:", difference)
+
+product = num1 * num2
+print("The product is:", product)
+
+if num2 != 0:
+    quotient = num1 / num2
+    print("The quotient is:", quotient)
+else:
+    print("Sorry, you can't divide by zero!")
