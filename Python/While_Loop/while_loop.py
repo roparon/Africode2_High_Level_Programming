059af4663b64b71c6d2494fcf1fd593179e8c59b
Aron Rop <aaronrop40@gmail.com>
2025-03-21 16:51:17 +0300

    innitial file

---
+++ Python/While_Loop/while_loop.py
@@ -0,0 +1,54 @@
+import time
+# counter = 0
+# while counter <= 5:
+#     counter += 1
+#     print(f"loop number:",counter)
+
+# username = input("Enter your username or Q to quit: ").lower()
+# list_of_banned_users = ("Rop", "Kirui", "Ben")
+
+# while username!="Q":
+#     print(f"Your username is {username}")
+#     username = input("Enter your username: ").lower()
+
+# x = 0
+# increment = True
+# while increment:
+#     print("x" * x)
+#     x += 1
+#     time.sleep(0.5)
+#     if x == 131:
+#         increment = False
+
+# char = "*"
+# count = 0
+# limit = 5
+
+# while True:
+#     print(char, end="")
+#     count += 1
+#     if count == limit:
+#         print()
+#         count = 0
+# i = 1
+# while i <= 10:
+#     j = 1
+#     while j <= 10:
+#         print(f"{i} x {j} = {i * j}")
+#         j += 1
+#     print("____________")
+#     i += 1
+
+# a = 2
+# while a <= 5:
+#     b = 1
+#     while b <= 5:
+#         print(f"{a} x {b} = {a * b}")
+#         b += 1
+#     print("____________")
+#     a += 1
+
+
+# Break Continue
+
+
