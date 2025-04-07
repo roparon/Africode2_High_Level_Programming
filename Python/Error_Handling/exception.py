a6cbcf71fa287326b1a9903ff7d482f68469b5a1
Aron Rop <aaronrop40@gmail.com>
2025-04-07 15:10:43 +0300

    error handling in python

--- Python/Error_Handling/exception.py
+++ Python/Error_Handling/exception.py
@@ -1,24 +1,59 @@
 # from math import kirui
 # print{"print_function"}
-a = 1
-b = 2
+# a = 1
+# b = 2
 
-def divide(a, b):
-    return a / b
+# def divide(a, b):
+#     return a / b
 
 # result = divide(1, 0)
 # print(result)
 
-try:
-    result = 1 / 0
-    print(result)
-except ZeroDivisionError as e:
-    print(f"Error {e}")
-except TypeError:
-    print("Invalid input")
-except:
-    print("An error occured")
-else:
-    print("Execution was successful!")
-finally:
-    ("I am being executed")
+# try:
+#     result = 1 / 0
+#     print(result)
+# except ZeroDivisionError as e:
+#     print(f"Error {e}")
+# except TypeError:
+#     print("Invalid input")
+# except:
+#     print("An error occured")
+# else:
+#     print("Execution was successful!")
+# finally:
+#     ("I am being executed")
+
+
+
+
+#*******************SYNTAX/COMPILE ***-- Happen when you've written code that Python doesn't understands
+#*******************intendation Error*****************************
+# def func():
+#     return None
+
+
+#******************Syntax Error ***********************************
+# def func():
+#     return None
+
+#******************Exception/Runtime Error*-----Happens during excecution of a programm
+# print("Hello" + 5)
+
+
+#**************************Name Error*******************************
+# print(Dorothy)
+#       other exception error
+"""
+- ValueError
+-ZeroDivisionError
+-KeyError
+-IndexError
+-FileNotFoundError
+"""
+
+#***********Logical Errors*********
+def sum(a,b):
+    return a/b
+result = sum (10, 3)
+print(result)
+
